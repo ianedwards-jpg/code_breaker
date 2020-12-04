0e01bac62fc11ed7e50a01e3ef834821f03a0060
Ian Edwards <ian.edward@uconn.edu>
2020-12-04 13:14:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,29 @@ function makeid(length) {
    return result;
 }
 
-console.log(makeid(3));
+
+//console.log("Test Value:" + makeid(3) + "/xxx");
+
+//Console Tests 
+
+console.log("https://www.dropbox.com/sh/" + makeid(3) + "/AABBc0MQI7Qtfd6JA2ZfI6lDa?dI=0");
+
+console.log("https://www.dropbox.com/sh/" + makeid(3) + "/AABBc0MQI7Qtfd6JA2ZfI6lDa?dI=0");
+
+
+//console.log(makeid(3));
+
+
+//URL Components 
+
+//var urlBegin = "https://www.dropbox.com/sh/" 
+
+//var urlMidFull = "6lt0ygrragwdeyw" 
+
+//var urlMidFirst = "6lt0yg
+
+//Var urlMidLast = "gwdeyw"
+
+//var urlEnd = "/AABBc0MQI7Qtfd6JA2ZfI6lDa?dI=0"
+
+//Test URL (Good URL) = "https://www.dropbox.com/sh/6lt0ygrragwdeyw/AABBc0MQI7Qtfd6JA2ZfI6lDa?dI=0"
